c69fa0ac25f93270093112f456c6e05ffac37c00
Tiago Gordiano de Souza <tiagogordiano@gmail.com>
2023-06-21 08:57:16 -0300

    add package de interfaces base

---
+++ Trab-Final/src/interfaces/IComandos.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface IComandos {
+    public void execute();
+}
---
+++ Trab-Final/src/interfaces/Subject.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+public interface Subject {
+    public void registrarObserver(Observer observer);
+    public void removerObserver(Observer observer);
+    public void notificarObservers();
+}
