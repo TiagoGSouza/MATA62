ab4dc56946b260b3e115305fddad8cdbe825d1bd
Tiago Gordiano de Souza <tiagogordiano@gmail.com>
2023-07-04 15:16:59 -0300

    corrigir adicao de exemplares
    Co-authored-by: igordantasgf <igordantasgf@users.noreply.github.com>

--- Trab-Final/src/controllers/BibliotecaFachada.java
+++ Trab-Final/src/controllers/BibliotecaFachada.java
@@ -51,8 +51,10 @@ public class BibliotecaFachada {
         livros.get(3).addExemplar(new Exemplar(livros.get(3), "05"));//201
         livros.get(4).addExemplar(new Exemplar(livros.get(4), "06"));//300
         livros.get(4).addExemplar(new Exemplar(livros.get(4), "07"));//300
-        livros.get(6).addExemplar(new Exemplar(livros.get(5), "08"));//400
-        livros.get(6).addExemplar(new Exemplar(livros.get(5), "09"));//400    
+        livros.get(6).addExemplar(new Exemplar(livros.get(6), "08"));//400
+        livros.get(6).addExemplar(new Exemplar(livros.get(6), "09"));//400
+        livros.get(6).addExemplar(new Exemplar(livros.get(6), "10"));//400    
+    
     }
 
     public void realizarEmprestimo(String codigoUsuario, String codigoLivro){
